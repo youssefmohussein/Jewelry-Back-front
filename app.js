95e847f330d00f79a5476eda72d5a6db537e0dbe
youssef mohamed <youssefhusseinnn1@gmail.com>
2025-05-07 17:04:18 +0300

    add

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 8000;
+const mongoose = require('mongoose');
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Joe');
 })
-app.listen(port, () => {
-    console.log('joe');
-})
+
+
+mongoose.connect("mongodb+srv://Jewelry-Ecommerce:xMEMzXiOVnkKpGIm@cluster0.bwsuhvy.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        app.listen(port, () => {
+            console.log('http://localhost:8000');
+        })
+    }).catch((err) => {
+        console.log(err)
+    });

a8bc8cf9cb2856fd3f5937072974dc2f32cc960e
Norhan Khaled <norasallam742@gmail.com>
2025-05-22 16:17:22 +0300

    add

--- models/users.js
+++ models/users.js
@@ -42,6 +42,12 @@ const usersManagementSchema = new mongoose.Schema({
   },
 });
 
+usersManagementSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 
 const users = mongoose.model('users', usersManagementSchema);
